5f0296cd4f9a19ea9bf4801743cd8793b60a8c0f
Naumenko Oleksandr <ddima1812000000@gmail.com>
2025-03-26 13:32:07 +0200

    Add files via upload

---
+++ lab08/prj/Naumenko_task_8_2/ModulesNaumenko.h
@@ -0,0 +1,8 @@
+#ifndef MODULESNAUMENKO_H_INCLUDED
+#define MODULESNAUMENKO_H_INCLUDED
+
+#include <cmath>
+
+double s_calculation(double x, double y, double z);
+
+#endif // MODULESNAUMENKO_H_INCLUDED
