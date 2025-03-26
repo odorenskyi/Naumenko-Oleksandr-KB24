bd16344ba5b4bd42255a285fa67007b666de3d28
Naumenko Oleksandr <ddima1812000000@gmail.com>
2025-03-26 14:35:02 +0200

    Delete lab08/prj directory

--- lab08/prj/Naumenko_task_8_2/ModulesNaumenko.h
+++
@@ -1,8 +0,0 @@
-#ifndef MODULESNAUMENKO_H_INCLUDED
-#define MODULESNAUMENKO_H_INCLUDED
-
-#include <cmath>
-
-double s_calculation(double x, double y, double z);
-
-#endif // MODULESNAUMENKO_H_INCLUDED
