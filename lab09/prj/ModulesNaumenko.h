c97531fbf81f17581ef9d93fe1fcc8c1cc1f1b33
Naumenko Oleksandr <ddima1812000000@gmail.com>
2025-04-08 20:42:20 +0300

    Changed ModulesNaumenko.h

--- lab09/prj/ModulesNaumenko.h
+++ lab09/prj/ModulesNaumenko.h
@@ -2,7 +2,19 @@
 #define MODULESNAUMENKO_H_INCLUDED
 
 #include <cmath>
+#include <vector>
 
+// Існуюча функція (за лабораторною №8)
 double s_calculation(double x, double y, double z);
+
+// Задача 9.1: розрахунок суми за спожиту електроенергію
+double calculateBill(int kWh);
+
+// Задача 9.2: визначення мінімального бала Бофорта
+int getBeaufortScale(double speed);
+int minBeaufortScale(const std::vector<double>& speeds);
+
+// Задача 9.3: аналіз бітів числа
+int countBits(unsigned int N);
 
 #endif // MODULESNAUMENKO_H_INCLUDED
