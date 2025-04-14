8f290500025edcea8cd83c07d3cdd78b5bbf1b9a
Naumenko Oleksandr <ddima1812000000@gmail.com>
2025-04-15 00:13:08 +0300

    Compiled a static library project

---
+++ lab10/prj/ModulesNaumenko.h
@@ -0,0 +1,11 @@
+#ifndef MODULES_NAUMENKO_H
+#define MODULES_NAUMENKO_H
+
+#include <string>
+
+void solveTask10_1(const std::string& inputFile, const std::string& outputFile);
+void solveTask10_2(const std::string& inputFile);
+void solveTask10_3(int x, int y, int z, int b, const std::string& outputFile);
+int s_calculation(int x, int y, int z);
+
+#endif
